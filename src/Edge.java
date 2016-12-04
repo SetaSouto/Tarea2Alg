3c43a9f9b2bbb90d84aebdc1d0f92fe3159522b0
Fabián Souto <souto_94@hotmail.com>
2016-12-04 16:18:08 -0300

    Changed the tag in Edge, now the tag is represented by the index of the first character and the last character of the tag in the real string S.

--- src/Edge.java
+++ src/Edge.java
@@ -1,59 +1,61 @@
 /**
  * Created by souto on 01-12-2016.
- *
  */
 public class Edge {
-    private String tag;
+    private int[] tag;
     private INode child;
 
     /**
      * Constructor.
-     * @param tag tag of the edge.
+     * The index represent the tag of the edge, if the tag is S[j..k], the index are i1=j, i2=k.
+     *
+     * @param i1    index of the first character of the tag.
+     * @param i2    index of the last character of the tag.
      * @param child child node.
      */
-    public Edge (String tag, INode child) {
-        this.tag = tag;
+    public Edge(int i1, int i2, INode child) {
+        this.tag = new int[]{i1, i2};
         this.child = child;
     }
 
     /**
-     * Returns the tag of the edge.
-     * @return the tag of the edge.
+     * Returns the index that represent the tag of the edge.
+     *
+     * @return the index of the tag.
      */
-    public String getTag() {
+    public int[] getTagIndex() {
         return this.tag;
     }
 
     /**
-     * Adds a character at the end of the tag.
-     * @param c
+     * Adds a character at the end of the tag. Its only need the index of the character in the
+     * real string S. If you want to add the character S[k] you give the int k.
+     *
+     * @param c index representing the character added to the tag.
      */
-    public void addChar(String c) {
-        if (c.length() > 1) {
-            throw new Error("String is not only one character.");
-        }
-        this.tag += c;
+    public void addCharIndex(int c) {
+        this.tag[1] = c;
     }
 
     /**
-     * Indicated the prefix of the tag to keep, splitEdge does a split of the edge, keeping prefix
-     * as the tag, created a new Node that is connected with the former child. The Edge connecting
-     * the new Node with the former Child has as tag the current tag without the prefix.
-     * @param prefix prefix to keep of the tag.
-     * @return the Node created during the split.
+     * Indicated the index where the tag must be divided (index is included in the first part of the
+     * split), splitEdge does a split of the edge, keeping as tag its first index and the index
+     * given like the last, and creates a new Node that is connected with the former child. The edge
+     * that connects the new Node with the former child has as tag the index given plus one, and
+     * the current last index, ie [untilIndex+1, this.tag[1]].
+     *
+     * @param untilIndex index until this edge has to keep its tag.
      */
-    public Node splitEdge(String prefix) {
-        if (this.tag.startsWith(prefix)) {
-            String newTag = this.tag.substring(prefix.length());
-            Edge newEdge = new Edge(newTag, this.child);
-            // Now, modify this Edge:
-            this.tag = prefix;
+    public Node splitEdge(int untilIndex) {
+        if (untilIndex > this.tag[0] && untilIndex < this.tag[1]) {
+            Edge newEdge = new Edge(untilIndex + 1, this.tag[1], this.child);
+            this.tag[1] = untilIndex;
             Node node = new Node();
             node.addEdge(newEdge);
             this.child = node;
             return node;
         } else {
-            throw new Error("String 'prefix' isn't a prefix of tag. Given: " + prefix + ", Tag: " + this.tag);
+            throw new Error("The index given is not between the current index of this edge's tag.");
         }
     }
 
